7110b5eb4b270558802aa2a0637cd7d2a507dcac
Shungo Yasuda <yasu39ktpr@au.com>
2025-06-25 19:33:55 +0900

    Modify assetprefix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/compare_vlms',
-  assetPrefix: '/compare_vlms/'
+  assetPrefix: '/compare_vlms',
 };
 
 export default nextConfig;
